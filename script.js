da4d22fac1899bfa58bca1366066925b68a700be
Trent_T <trenttuedon2000@gmail.com>
2023-06-07 17:37:54 -0400

    Finished Desktop Design

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector(".hamburger-menu"); 
 const iconMenu = document.querySelector(".icon-menu");
 const menuItems = document.querySelector(".menu-items"); 
+const backgroundImage = document.querySelector(".background-image"); 
+
+const body = document.querySelector("body"); 
 iconMenu.addEventListener("click", () => {
     hamburger.classList.toggle("menu-box"); 
     iconMenu.classList.toggle("toggle-icon"); 
     menuItems.classList.toggle("toggle-menu-items"); 
+    body.classList.toggle("toggle-dark");
+    backgroundImage.classList.toggle("toggle-background-brightness");
 })
 iconMenu.src = "assets/images/icon-menu.svg"; 
